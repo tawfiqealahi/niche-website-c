9be7420e8f9fea300e1fdf79f14856d808b32b2e
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-11-13 18:06:52 -0600

    5

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './components/Home/Home/Home';
-import Error from './components/Error/Error';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import LogIn from './components/LogIn/LogIn';
-import Register from './components/Register/Register';
-import AuthProvider from './context/AuthProvider';
-import AllProducts from './components/AllProducts/AllProducts';
-import OurAbout from './components/OurAbout/OurAbout';
-import Dashboard from './components/Dashboard/Dashboard/Dashboard';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import Order from './components/Order/Order';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home/Home";
+import Error from "./components/Error/Error";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import LogIn from "./components/LogIn/LogIn";
+import Register from "./components/Register/Register";
+import AuthProvider from "./context/AuthProvider";
+import AllProducts from "./components/AllProducts/AllProducts";
+import OurAbout from "./components/OurAbout/OurAbout";
+import Dashboard from "./components/Dashboard/Dashboard/Dashboard";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Order from "./components/Order/Order";
 
 function App() {
   return (
-      <AuthProvider>
-        <Router>
-          <Header></Header>
-            <Switch>
-              <Route exact path="/">
-                <Home></Home>
-              </Route>
-              <Route path="/home">
-                <Home></Home>
-              </Route>
-              <PrivateRoute path="/dashboard">
-                <Dashboard></Dashboard>
-              </PrivateRoute>
-              <PrivateRoute path="/allProducts">
-                <AllProducts></AllProducts>
-              </PrivateRoute>
-              <PrivateRoute path="/products/:id">
-                <Order></Order>
-              </PrivateRoute>
-              <Route path="/about">
-                <OurAbout></OurAbout>
-              </Route>
-              <Route path="/logIn">
-                <LogIn></LogIn>
-              </Route>
-              <Route path="/register">
-                <Register></Register>
-              </Route>
-              <Route path="*">
-                <Error></Error>
-              </Route>
-            </Switch>
-          <Footer></Footer>
-        </Router>
-      </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <PrivateRoute path="/dashboard">
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+          <PrivateRoute path="/allProducts">
+            <AllProducts></AllProducts>
+          </PrivateRoute>
+          <PrivateRoute path="/products/:id">
+            <Order></Order>
+          </PrivateRoute>
+          <Route path="/about">
+            <OurAbout></OurAbout>
+          </Route>
+          <Route path="/logIn">
+            <LogIn></LogIn>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+          <Route path="*">
+            <Error></Error>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,11 @@
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGWn6JzMQOkZqvb7x9QzM2m0G1ruKroiQ",
-  authDomain: "niche-product12.firebaseapp.com",
-  projectId: "niche-product12",
-  storageBucket: "niche-product12.appspot.com",
-  messagingSenderId: "189367765556",
-  appId: "1:189367765556:web:e8b7af849d53c2d9969861"
+  apiKey: "AIzaSyDp4jiS2OB8e2dJrjkYpYZYfImjp9ULDTI",
+  authDomain: "niche-product-2a756.firebaseapp.com",
+  projectId: "niche-product-2a756",
+  storageBucket: "niche-product-2a756.appspot.com",
+  messagingSenderId: "819839693028",
+  appId: "1:819839693028:web:49ed8f99af581a24917cf1",
 };
 
 export default firebaseConfig;
