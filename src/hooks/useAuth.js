7ca627b99c9c3db3bdb1ae529a5f6a86b3658a2b
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-11-22 04:27:37 -0600

    fj5

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
 
 const useAuth = () => {
+
     return useContext(AuthContext);
+    
 };
 
 export default useAuth;
